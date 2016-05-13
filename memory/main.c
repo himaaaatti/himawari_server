438770a2e296538c922c030336622106a68295e0
himaaaatti <sux2mfgj@gmail.com>
2016-05-13 19:55:13 +0900

    regist own to server

--- memory/main.c
+++ memory/main.c
@@ -1,18 +1,43 @@
 #include <task.h>
 #include <stdbool.h>
+#include <server.h>
+
+#define EARLY_MEMORY_PAGE_NUM 64
+#define bitmap_size ((EARLY_MEMORY_PAGE_NUM + 64 - 1) >> 6)
+
+uint64_t bitmap[bitmap_size];
+
+static void initialize(void)
+{
+    struct Message msg;
+    uint64_t result; 
+
+    // Registing server to System
+    msg.number = Regist;
+    msg.content.num = Memory;
+    result = send(System, &msg);
+
+    // get early memory map
+    msg.dest = Memory;
+    msg.number = Initialize,
+    msg.content.address = (uintptr_t)bitmap;
+    msg.content.num = bitmap_size * 8; // bitmap end
+
+    result = send(System, &msg);
+
+    msg.content.address = 0;
+    msg.content.num = 0;
+    result = receive(System, &msg);
+}
 
 void main(void)
 {
     uint64_t result; 
-    struct Message msg = {
-        .source = Memory,
-        .dest = System,
-        .type = Receive,
-        .content = {0,0},
-    };
 
-    result = receive(&msg);
+    initialize();
+
+    struct Message msg;
     while (true) {
-        
+        result = receive(Any, &msg);
     }
 }
