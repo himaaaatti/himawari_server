d2b235d71af24ecccca19507b90f6af06df66ac6
himaaaatti <sux2mfgj@gmail.com>
2016-05-16 13:16:20 +0900

    new protocol

--- include/task.h
+++ include/task.h
@@ -2,7 +2,3 @@
 #include <stdint.h>
 #include <task_call.h>
 
-
-extern uint64_t receive(struct Message* msg);
-extern uint64_t send(struct Message *msg);
-
--- memory/main.c
+++ memory/main.c
@@ -1,18 +1,41 @@
 #include <task.h>
 #include <stdbool.h>
+#include <server.h>
+
+#define PAGE_SIZE 0x1000
+#define EARLY_MEMORY_SIZE 0x01000000 // 16MB (after ISA memory hole)
+#define EARLY_MEMORY_PAGE_NUM (EARLY_MEMORY_SIZE / PAGE_SIZE)
+#define BIT_NUMBER_OF_BYTE 8
+#define BITMAP_UNIT64_NUM (EARLY_MEMORY_PAGE_NUM / ((sizeof(uint64_t) * BIT_NUMBER_OF_BYTE)))
+#define BITMAP_ENTRY_NUM BITMAP_UNIT64_NUM
+
+uint64_t bitmap[BITMAP_ENTRY_NUM];
+
+static void initialize(void)
+{
+    struct Message msg;
+    msg.dest = Memory;
+    msg.number = Initialize,
+    msg.content.address = (uintptr_t)bitmap;
+    msg.content.num = BITMAP_ENTRY_NUM * 8; // bitmap end
+
+    uint64_t result;
+    result = send(System, &msg);
+
+    msg.content.address = 0;
+    msg.content.num = 0;
+    result = receive(System, &msg);
+
+    uintptr_t physical_end_addr = msg.content.end_address;
+}
 
 void main(void)
 {
     uint64_t result; 
-    struct Message msg = {
-        .source = Memory,
-        .dest = System,
-        .type = Receive,
-        .content = {0,0},
-    };
+    initialize();
 
-    result = receive(&msg);
+    struct Message msg;
     while (true) {
-        
+        result = receive(Any, &msg);
     }
 }
